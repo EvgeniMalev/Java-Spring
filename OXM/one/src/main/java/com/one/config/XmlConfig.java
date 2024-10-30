f07dfb1da8e73d83460390e48d38eba21eb8516e
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-10-30 23:19:31 +0200

    Create XmlConfig.java

---
+++ OXM/one/src/main/java/com/one/config/XmlConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class XmlConfig {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setPackagesToScan("com.example.demo.model");
+        return marshaller;
+    }
+}
