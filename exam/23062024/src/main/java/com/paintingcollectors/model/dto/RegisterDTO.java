2183c5751f43d3ce617249f318467c5ebf65ad49
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2025-07-24 23:05:20 +0300

    Create RegisterDTO.java

---
+++ exam/23062024/src/main/java/com/paintingcollectors/model/dto/RegisterDTO.java
@@ -0,0 +1,75 @@
+package com.paintingscollectors.model.dto;
+
+import com.paintingscollectors.vallidation.annotation.UniqueEmail;
+import com.paintingscollectors.vallidation.annotation.UniqueUsername;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class RegisterDTO {
+
+    private Long id;
+
+    @UniqueUsername
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters!")
+    @NotNull
+    private String username;
+
+    @UniqueEmail
+    @Email(message = "Enter valid email!")
+    @NotBlank(message = "Cannot be empty!")
+    private String email;
+
+    @Size(min = 3, max = 20, message = "Password must be between 3 and 20 characters!")
+    @NotNull
+    private String password;
+
+    @Size(min = 3, max = 20, message = "Password must be between 3 and 20 characters!")
+    @NotNull
+    private String confirmPassword;
+
+    public RegisterDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
